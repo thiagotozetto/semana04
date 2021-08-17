777d648d8b759ae928dd9298ed8a754262d94087
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2021-08-17 11:33:38 -0300

    conta corrente implementada.

--- semana04/src/App.java
+++ semana04/src/App.java
@@ -1,5 +1,40 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //criando uma nova variavel do tipo ContaCorrente
+        ContaCorrente conta, conta2;
+
+        //instanciar
+        conta = new ContaCorrente();
+        conta2 = new ContaCorrente();
+
+        System.out.println(conta);
+
+        //inicializando a instancia
+        conta.agencia = "0101-1";
+        conta.numero = "897610-1";
+        conta.cpf = "087.564.123-00";
+        conta.saldo = 1000.0;
+
+        //inicializando a instancia
+        conta2.agencia = "2101-2";
+        conta2.numero = "2897610-2";
+        conta2.cpf = "287.564.123-02";
+        conta2.saldo = 100.0;
+        
+        System.out.println(conta);
+        System.out.println(conta2);
+
+        conta.sacar(50.0);
+        conta.depositar(500);
+        
+        conta.transferir(conta2, 600.0);
+
+        System.out.println(conta.toString());
+        System.out.println(conta2.toString());
+
+
+
+    
     }
 }
---
+++ semana04/src/ContaCorrente.java
@@ -0,0 +1,41 @@
+//Nome
+public class ContaCorrente {
+    
+    //atributos
+    String agencia;
+    String numero;
+    String cpf;
+    double saldo;
+
+    //métodos
+    
+    //sacar
+    boolean sacar(double qtde){
+        saldo = saldo - qtde;
+        return true;
+    }
+
+    //depositar
+    void depositar(double qtde){
+        saldo = saldo + qtde;
+    }
+
+    //transferir
+    boolean transferir(ContaCorrente destino, double qtde){
+        sacar(qtde);
+        destino.depositar(qtde);
+        return true;
+    }
+    
+    //versaldo
+    double verSaldo(){
+        return saldo;
+    }
+
+    public String toString(){
+        String texto="";
+        texto = "Agencia: "+agencia+" Numero:"+numero+" Cpf:"+cpf+" Saldo R$:"+saldo;
+        return texto;
+    }
+
+}
